e840afe583cb24c05c8a4a22a227e9b63406e87d
wys <1394945493@qq.com>
2026-09-17 21:38:35 +0800

    修复代码

--- run.sh
+++ run.sh
@@ -86,7 +86,7 @@ torchrun --nproc_per_node=2 --master_port=29501 \
 #  火山服务器训练
 
 PYTHONPATH="$(pwd)" \
-python -m torch.distributed.launch \
+torchrun \
     --nproc_per_node="${MLP_WORKER_GPU}" \
     --master_addr="${MLP_WORKER_0_HOST}" \
     --node_rank="${MLP_ROLE_INDEX}" \
--- tools/train.py
+++ tools/train.py
@@ -158,7 +158,7 @@ def parse_args():
         choices=['none', 'pytorch', 'slurm', 'mpi'],
         default='none',
         help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--local_rank', '--local-rank', type=int, default=0)
     parser.add_argument(
         '--autoscale-lr',
         action='store_true',
